00a9496ebdd1dffe479e295ce33703d13a1a26fb
Damien JARRY <dams_terdam@hotmail.fr>
2018-10-26 15:32:44 +0200

    Some cleaning

--- src/renderer.ts
+++ src/renderer.ts
@@ -9,6 +9,8 @@ export interface Result {
 
 export class Renderer {
   private readonly browser: Browser;
+  private page: Page;
+  private response: Response;
 
   constructor(browser: Browser) {
     this.browser = browser;
@@ -51,32 +53,21 @@ export class Renderer {
   }
 
   async init(requestUrl: string): Promise<Result> {
-    const page: Page = await this.browser.newPage();
-    await page.setViewport({width: 375, height: 667, isMobile: true, hasTouch: true, deviceScaleFactor: 2});
-    await page.evaluateOnNewDocument('customElements.forcePolyfill = true');
-    await page.evaluateOnNewDocument('ShadyDOM = {force: true}');
-    await page.evaluateOnNewDocument('ShadyCSS = {shimcssproperties: true}');
+    this.response = null;
+    this.page = await this.browser.newPage();
+    await this.page.setViewport({width: 375, height: 667, isMobile: true, hasTouch: true, deviceScaleFactor: 2});
+    await this.page.evaluateOnNewDocument('customElements.forcePolyfill = true');
+    await this.page.evaluateOnNewDocument('ShadyDOM = {force: true}');
 
-    let response: Response = null;
-    page.addListener('response', (r) => {
-      if (!response) {
-        response = r;
+    await this.page.evaluateOnNewDocument('ShadyCSS = {shimcssproperties: true}');
+    this.page.addListener('response', (r) => {
+      if (!this.response) {
+        this.response = r;
       }
     });
-    try {
-      // Navigate to page. Wait until there are no outstanding network requests.
-      response = await page.goto(requestUrl, {
-        timeout: 10000, waitUntil: [
-          'load',
-          'domcontentloaded',
-          'networkidle0'
-        ]
-      });
-    } catch (e) {
-      console.error(e);
-    }
+    await this.gotoUrl(requestUrl);
 
-    if (!response) {
+    if (!this.response) {
       console.error('response does not exist');
       // This should only occur when the page is about:blank. See
       // https://github.com/GoogleChrome/puppeteer/blob/v1.5.0/docs/api.md#pagegotourl-options.
@@ -85,31 +76,40 @@ export class Renderer {
 
     // Disable access to compute metadata. See
     // https://cloud.google.com/compute/docs/storing-retrieving-metadata.
-    if (response.headers()['metadata-flavor'] === 'Google') {
+    if (this.response.headers()['metadata-flavor'] === 'Google') {
       return {status: 403, content: ''};
     }
 
-    let statusCode = response.status();
-    // On a repeat visit to the same origin, browser cache is enabled, so we may
-    // encounter a 304 Not Modified. Instead we'll treat this as a 200 OK.
-    if (statusCode === 304) {
-      statusCode = 200;
-    }
-
     // Remove script & import tags.
-    await page.evaluate(Renderer.stripPage);
+    await this.page.evaluate(Renderer.stripPage);
     // Inject <base> tag with the origin of the request (ie. no path).
     const parsedUrl: UrlWithStringQuery = url.parse(requestUrl);
-    await page.evaluate(Renderer.injectBaseHref, `${parsedUrl.protocol}//${parsedUrl.host}`);
+    await this.page.evaluate(Renderer.injectBaseHref, `${parsedUrl.protocol}//${parsedUrl.host}`);
 
-    const result: string = await page.evaluate(() => {
+    const content: string = await this.page.evaluate(() => {
       return (new XMLSerializer().serializeToString(document.doctype) + document.firstElementChild.outerHTML)
     });
-    await page.close();
+    await this.page.close();
 
     return {
-      status: statusCode,
-      content: result
+      status: this.response.status() === 304 ? 200 : this.response.status(),
+      content
     };
+  }
+
+  private async gotoUrl(requestUrl) {
+    try {
+      // Navigate to page. Wait until there are no outstanding network requests.
+      this.response = await this.page.goto(requestUrl, {
+        timeout: 10000, waitUntil: [
+          'load',
+          'domcontentloaded',
+          'networkidle0'
+        ]
+      });
+    } catch (e) {
+      console.error(e);
+      this.response = null;
+    }
   }
 }
